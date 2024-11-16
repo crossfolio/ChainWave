ff6400495d802ad0eaf7e39d0ac3abb1e11b067f
ben <cjay41107@gmail.com>
2024-11-16 09:58:28 +0800

    feat: user crud and mongodb and chores

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,14 +1,33 @@
 import express, { Request, Response } from 'express'
+import './controllers/CronJobController'
+import swaggerDocument from '../dist/swagger.json'
+import swaggerUi from 'swagger-ui-express'
 import dotenv from 'dotenv'
+import { RegisterRoutes } from './routes/routes'
+import { errorHandler } from './middlewares/errorHandler'
 
 dotenv.config()
 
 const app = express()
+
+// Middleware
+app.use(express.json())
 
 app.get('/', (_req: Request, res: Response) => {
   res.status(200).send({
     message: 'Hello World!',
   })
 })
+
+// Swagger
+app.use('/api/docs', swaggerUi.serve, async (_req: Request, res: Response) => {
+  res.send(swaggerUi.generateHTML(swaggerDocument))
+})
+
+// TSOA Routes
+RegisterRoutes(app)
+
+// Error Handling Middleware
+app.use(errorHandler)
 
 export default app
