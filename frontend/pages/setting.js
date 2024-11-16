d630584d71a24a4b48c1d341c65aabf96b696dab
York <a0930851137@gmail.com>
2024-11-16 15:37:01 +0700

    feat_frontend: added apis

--- frontend/pages/setting.js
+++ frontend/pages/setting.js
@@ -1,212 +1,335 @@
 import { useState, useEffect } from 'react';
-import axios from 'axios';
 import Cookies from 'js-cookie';
+import { formatAddress } from '../utils/util';
+import { queryAttestationsId } from '../utils/signProtocol';
+import { SignProtocolClient, SpMode, EvmChains } from '@ethsign/sp-sdk';
 
-export default function NotificationList({ worldcoinid }) {
-  const [notifications, setNotifications] = useState([]);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
-  const [isEditDialogOpen, setIsEditDialogOpen] = useState(false);
-  const [editNotification, setEditNotification] = useState(null);
+export default function SettingPage() {
+  const [isEditing, setIsEditing] = useState(false);
+  const [worldcoinId, setWorldcoinId] = useState('');
+  const [accountAddress, setAccountAddress] = useState('');
+  const [profileImage, setProfileImage] = useState(null);
+  const [revokeConfirmation, setRevokeConfirmation] = useState('');
+  const [isMessageSigned, setIsMessageSigned] = useState(false);
+  const [isAttestationCreated, setIsAttestationCreated] = useState(false);
+  const [schemaId, setSchemaId] = useState('0x14e');
+  let [username, setUsername] = useState('');
 
   useEffect(() => {
-    const worldcoinid = Cookies.get('worldcoinId');
-    if (!worldcoinid) return;
+    const fetchUserInfo = async () => {
+      const savedWorldcoinId = Cookies.get('worldcoinId');
+      if (savedWorldcoinId) {
+        setWorldcoinId(savedWorldcoinId);
+      }
 
-    const fetchNotifications = async () => {
-      setLoading(true);
-      setError(null);
-      try {
-        const response = await fetch(
-          `/api/notifications?worldcoinid=${worldcoinid}`,
-          {
-            method: 'GET',
-            headers: {
-              'Content-Type': 'application/json',
-            },
-          },
-        );
-        const data = await response.json();
-        setNotifications(data);
-      } catch (err) {
-        setError('Failed to load notifications.');
-      } finally {
-        setLoading(false);
+      const userInfo = await getUserInfo(savedWorldcoinId);
+      if (userInfo && userInfo.name) {
+        setUsername(userInfo.name);
+      }
+
+      if (window.ethereum) {
+        window.ethereum
+          .request({ method: 'eth_requestAccounts' })
+          .then((accounts) => {
+            setAccountAddress(accounts[0]);
+          })
+          .catch((error) => {
+            console.error('Error connecting to MetaMask:', error);
+          });
+      } else {
+        alert('Please install MetaMask to connect your wallet.');
       }
     };
 
-    fetchNotifications();
-  }, [worldcoinid]);
+    fetchUserInfo();
+  }, []);
 
-  const handleDelete = async (id) => {
-    try {
-      await axios.delete(`/api/notifications/${worldcoinid}/${id}`);
-      setNotifications(
-        notifications.filter((notification) => notification.id !== id),
+  useEffect(() => {
+
+    const updatedProfileImage = `https://noun-api.com/beta/pfp?name=${encodeURIComponent(username)}`;
+    setProfileImage(updatedProfileImage);
+  }, [username]);
+
+  const handleSave = async () => {
+    const worldcoinId = Cookies.get('worldcoinId');
+    if (worldcoinId) {
+      await updateUserInfo(worldcoinId);
+    }
+    setIsEditing(false);
+  };
+
+  const client = new SignProtocolClient(SpMode.OnChain, {
+    chain: EvmChains.arbitrumSepolia,
+  });
+
+  const createAttestation = async () => {
+    const worldcoinId = Cookies.get('worldcoinId');
+    if (!window.ethereum || !accountAddress || !schemaId || !worldcoinId) {
+      alert(
+        'Please ensure all fields are filled in, and MetaMask is connected',
       );
-    } catch (err) {
-      console.error('Error deleting notification:', err);
-      alert('Failed to delete notification.');
+      return;
+    }
+
+    try {
+      const message = JSON.stringify({
+        worldcoinId: worldcoinId,
+      });
+
+      const signedMessage = await window.ethereum.request({
+        method: 'personal_sign',
+        params: [message, accountAddress],
+      });
+
+      setIsMessageSigned(true);
+
+      const attestationData = {
+        worldcoinSign: signedMessage,
+      };
+
+      const attestationRes = await client.createAttestation({
+        schemaId: schemaId,
+        data: attestationData,
+      });
+
+      setIsAttestationCreated(true);
+
+      console.log('Attestation Created:', attestationRes);
+      alert('Attestation Created', attestationRes);
+    } catch (error) {
+      console.error('Failed to create attestation:', error);
+      alert('Failed to create attestation');
     }
   };
 
-  const openEditDialog = (notification) => {
-    setEditNotification(notification);
-    setIsEditDialogOpen(true);
-  };
+  const revokeAttestation = async () => {
+    if (!revokeConfirmation || revokeConfirmation !== worldcoinId) {
+      alert('Please enter worldcoin ID to confirm revoke');
+      return;
+    }
 
-  const closeEditDialog = () => {
-    setIsEditDialogOpen(false);
-    setEditNotification(null);
-  };
-
-  const handleEditChange = (e) => {
-    const { name, value } = e.target;
-    setEditNotification((prev) => ({ ...prev, [name]: value }));
-  };
-
-  const handleEditSubmit = async () => {
     try {
-      await axios.put(
-        `/api/notifications/${worldcoinid}/${editNotification.id}`,
-        editNotification,
+      const querySchemaId = 'onchain_evm_421614_0x14e';
+      const attestationId = await queryAttestationsId(
+        querySchemaId,
+        worldcoinId,
       );
-      setNotifications((prev) =>
-        prev.map((notif) =>
-          notif.id === editNotification.id ? editNotification : notif,
-        ),
+      console.log('Revoke Attestation ID:', attestationId);
+      const response = await client.revokeAttestation(attestationId, {
+        reason: 'User requested revocation',
+      });
+      console.log('Attestation Revoked:', response);
+      alert('Attestation has been successfully revoked.');
+    } catch (error) {
+      console.error('Failed to revoke attestation:', error);
+      alert('Failed to revoke attestation. Check the console for details.');
+    }
+  };
+
+  const getUserInfo = async (worldcoinId) => {
+    try {
+      const response = await fetch(
+        `http://localhost:3001/api/users/${worldcoinId}`,
+        {
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        },
       );
-      closeEditDialog();
-    } catch (err) {
-      console.error('Error updating notification:', err);
-      alert('Failed to update notification.');
+
+      const data = await response.json();
+      console.log(data);
+
+      return data;
+    } catch (error) {
+      console.error('Error during get user info:', error);
+    }
+  };
+
+  const updateUserInfo = async (worldcoinId) => {
+    try {
+      const response = await fetch(
+        `http://localhost:3001/api/users/${worldcoinId}`,
+        {
+          method: 'PUT',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+            name: username,
+          }),
+        },
+      );
+
+      const data = await response.json();
+      if (data.code === 200) {
+        alert('Changes saved!');
+        window.location.reload();
+        console.log('Update user successfully');
+      } else {
+        alert('Error during update user info!');
+      }
+    } catch (error) {
+      console.error('Error during update user info:', error);
+      alert('Error during update user info!');
     }
   };
 
   return (
-    <div className="container mx-auto px-4 py-8">
-      <h2 className="text-3xl font-bold text-gray-800 mb-6 text-center">
-        Notification List
+    <div className="w-full min-h-screen bg-gray-100 p-8">
+      <h2 className="text-3xl font-bold text-gray-800 mb-10 text-center">
+        Account Settings
       </h2>
 
-      {loading ? (
-        <p>Loading...</p>
-      ) : error ? (
-        <p className="text-red-500">{error}</p>
-      ) : (
-        <div className="overflow-x-auto">
-          <table className="w-full max-w-5xl mx-auto bg-white shadow-md rounded-lg overflow-hidden">
-            <thead className="bg-gray-800 text-white">
-              <tr>
-                <th className="py-3 px-4 text-left">#</th>
-                <th className="py-3 px-4 text-left">Symbol</th>
-                <th className="py-3 px-4 text-left">Condition</th>
-                <th className="py-3 px-4 text-left">Price</th>
-                <th className="py-3 px-4 text-center">Actions</th>
-              </tr>
-            </thead>
-            <tbody>
-              {Array.isArray(notifications) && notifications.length > 0 ? (
-                notifications.map((notification, index) => (
-                  <tr key={notification.id} className="border-b">
-                    <td className="py-3 px-4">{index + 1}</td>
-                    <td className="py-3 px-4 font-semibold text-gray-700">
-                      {notification.symbol}
-                    </td>
-                    <td className="py-3 px-4">
-                      {notification.condition === 'greater' ? '>' : '<'}
-                    </td>
-                    <td className="py-3 px-4">${notification.price}</td>
-                    <td className="py-3 px-4 text-center">
-                      <button
-                        onClick={() => openEditDialog(notification)}
-                        className="bg-blue-500 text-white px-3 py-1 rounded hover:bg-blue-600 mr-2"
-                      >
-                        Edit
-                      </button>
-                      <button
-                        onClick={() => handleDelete(notification.id)}
-                        className="bg-red-500 text-white px-3 py-1 rounded hover:bg-red-600"
-                      >
-                        Delete
-                      </button>
-                    </td>
-                  </tr>
-                ))
+      <div className="bg-white p-8 rounded-lg shadow-lg mx-auto w-full max-w-4xl mb-10">
+        {/* Profile Image Section */}
+        <div className="flex items-center space-x-6 mb-6">
+          <div className="relative">
+            <div className="w-24 h-24 rounded-full overflow-hidden bg-gray-300">
+              {profileImage ? (
+                <img
+                  src={profileImage}
+                  alt="Profile"
+                  className="w-full h-full object-cover"
+                />
               ) : (
-                <tr>
-                  <td colSpan="5" className="py-4 text-center text-gray-500">
-                    {loading ? 'Loading...' : 'No notifications found.'}
-                  </td>
-                </tr>
+                <span className="flex items-center justify-center text-2xl font-semibold text-gray-500">
+                  JD
+                </span>
               )}
-            </tbody>
-          </table>
+            </div>
+          </div>
+          <div>
+            <p className="text-gray-600">Profile Picture</p>
+          </div>
         </div>
-      )}
 
-      {isEditDialogOpen && (
-        <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50">
-          <div className="bg-white p-6 rounded-lg shadow-lg w-full max-w-md">
-            <h3 className="text-xl font-bold text-gray-800 mb-4">
-              Edit Notification
-            </h3>
-            <div className="mb-4">
-              <label className="block text-sm font-medium text-gray-700">
-                Symbol
-              </label>
-              <input
-                type="text"
-                name="symbol"
-                value={editNotification.symbol}
-                onChange={handleEditChange}
-                className="w-full border rounded-lg p-2"
-                disabled
-              />
-            </div>
-            <div className="mb-4">
-              <label className="block text-sm font-medium text-gray-700">
-                Condition
-              </label>
-              <select
-                name="condition"
-                value={editNotification.condition}
-                onChange={handleEditChange}
-                className="w-full border rounded-lg p-2"
-              >
-                <option value="greater">Greater than</option>
-                <option value="less">Less than</option>
-              </select>
-            </div>
-            <div className="mb-4">
-              <label className="block text-sm font-medium text-gray-700">
-                Price
-              </label>
-              <input
-                type="number"
-                name="price"
-                value={editNotification.price}
-                onChange={handleEditChange}
-                className="w-full border rounded-lg p-2"
-              />
-            </div>
-            <div className="flex justify-end mt-6 space-x-4">
+        {/* Username */}
+        <div className="mb-6">
+          <label className="block text-sm font-medium text-gray-700">
+            Username
+          </label>
+          {isEditing ? (
+            <input
+              type="text"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+              className="mt-1 w-full p-3 border border-gray-300 rounded-lg focus:outline-none focus:border-blue-500"
+              placeholder="Enter your username"
+            />
+          ) : (
+            <p className="text-lg text-gray-800">{username}</p>
+          )}
+        </div>
+
+        {/* Worldcoin ID */}
+        <div className="mb-6">
+          <label className="block text-sm font-medium text-gray-700">
+            Worldcoin ID
+          </label>
+          <p className="text-lg text-gray-800">{worldcoinId}</p>
+        </div>
+
+        {/* Account Address */}
+        <div className="mb-6">
+          <label className="block text-sm font-medium text-gray-700">
+            Account Address
+          </label>
+          <p className="text-lg text-gray-800 break-words">
+            {accountAddress ? formatAddress(accountAddress) : 'Not connected'}
+          </p>
+        </div>
+
+        {/* Edit and Save Buttons */}
+        <div className="flex space-x-4 mt-6">
+          {isEditing ? (
+            <>
               <button
-                onClick={handleEditSubmit}
-                className="bg-blue-500 text-white px-4 py-2 rounded-lg hover:bg-blue-600"
+                onClick={handleSave}
+                className="bg-blue-500 text-white py-2 px-6 rounded-lg font-medium hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-400"
               >
-                Confirm
+                Save Changes
               </button>
               <button
-                onClick={closeEditDialog}
-                className="bg-gray-300 px-4 py-2 rounded-lg hover:bg-gray-400"
+                onClick={() => setIsEditing(false)}
+                className="bg-gray-300 text-gray-700 py-2 px-6 rounded-lg font-medium hover:bg-gray-400 focus:outline-none focus:ring-2 focus:ring-gray-300"
               >
                 Cancel
               </button>
-            </div>
-          </div>
+            </>
+          ) : (
+            <button
+              onClick={() => setIsEditing(true)}
+              className="bg-blue-500 text-white py-2 px-6 rounded-lg font-medium hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-400"
+            >
+              Edit Profile
+            </button>
+          )}
         </div>
-      )}
+      </div>
+
+      {/* Create Attestation Section */}
+      <div className="bg-white p-8 rounded-lg shadow-lg mx-auto w-full max-w-4xl mb-10">
+        <h3 className="text-2xl font-semibold text-gray-700 mb-6">
+          Create Attestation
+        </h3>
+        <div className="flex items-center mb-4">
+          <p
+            className={`mr-2 ${isMessageSigned ? 'text-green-500' : 'text-gray-500'}`}
+          >
+            Sign message
+          </p>
+          {isMessageSigned ? (
+            <span className="text-green-500 font-semibold">✔</span>
+          ) : (
+            <span className="text-gray-500 font-semibold">✗</span>
+          )}
+        </div>
+
+        <div className="flex items-center mb-4">
+          <p
+            className={`mr-2 ${isAttestationCreated ? 'text-green-500' : 'text-gray-500'}`}
+          >
+            Create Attestation
+          </p>
+          {isAttestationCreated ? (
+            <span className="text-green-500 font-semibold">✔</span>
+          ) : (
+            <span className="text-gray-500 font-semibold">✗</span>
+          )}
+        </div>
+
+        <button
+          onClick={createAttestation}
+          className="bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-400"
+        >
+          Create Attestation
+        </button>
+      </div>
+
+      {/* Revoke Attestation Section */}
+      <div className="bg-white p-6 rounded-lg shadow-lg mx-auto w-full max-w-4xl">
+        <h3 className="text-2xl font-semibold text-gray-700 mb-6">
+          Revoke Attestation
+        </h3>
+        <label className="block text-sm font-small text-gray-700 mb-2">
+          Please enter the worldcoin ID to revoke
+        </label>
+        <input
+          type="text"
+          value={revokeConfirmation}
+          onChange={(e) => setRevokeConfirmation(e.target.value)}
+          className="w-full p-3 border border-gray-300 rounded-lg focus:outline-none focus:border-blue-500 mb-4"
+          placeholder="Enter worldcoin ID"
+        />
+        <button
+          onClick={revokeAttestation}
+          className="bg-red-500 text-white py-2 px-6 rounded-lg font-medium hover:bg-red-600 focus:outline-none focus:ring-2 focus:ring-red-400"
+        >
+          Revoke Attestation
+        </button>
+      </div>
     </div>
   );
 }
